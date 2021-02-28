8318a2a61e47a0fa000fea85f62f2288bccf3573
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-02-28 21:31:26 +0100

    Add notebook with idioms&patterns in Python

---
+++ Idioms&patterns.py
@@ -0,0 +1,16 @@
+#Different comprehensions
+
+#List comprehension
+[x for x in range(0,5)]
+
+#Set comprehension
+{x for x in range(0,5)}
+
+#Dictionary comprehension
+{x:x for x in range(0,5)}
+
+#Generator expression
+(x for x in range(0,5))
+
+
+print((x for x in range(0,5)))
